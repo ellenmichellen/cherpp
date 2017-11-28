f2503becf110b5ed5b90cdd5056527445b09cd83
ellenmichellen <ellen.coombs@dcmail.ca>
2017-11-28 17:12:02 -0500

    Renamed image and fixed styling on HEIT 2018 page

--- heit-symposia.php
+++ heit-symposia.php
@@ -5,11 +5,10 @@
 
 get_header();
 
-$page_header = get_field('page_header');					// Variable to store page header
-$page_subheader = get_field('page_subheader');				// Variable to store page subheader
-$main_content = get_field('main_content');					// Variable to store main content
 $heit_2015_text = get_field('heit_2015_text');				// Variable to store HEIT 2015 text
+$heit_2015_image = get_field('heit_2015_image');			// Variable to store HEIT 2015 image
 $heit_2016_text = get_field('heit_2016_text');				// Variable to store HEIT 2016 text
+$heit_2016_image = get_field('heit_2016_image');			// Variable to store HEIT 2016 image
 
 ?>
 
@@ -18,7 +17,7 @@ $heit_2016_text = get_field('heit_2016_text');				// Variable to store HEIT 2016
 <section id="slider" class="slider-parallax" style="background: linear-gradient(rgba(46, 179, 214, 0.70), rgba(67, 204, 158, 0.70)), url('<?php  bloginfo('template_url');  ?>/images/speakers.jpg?>') no-repeat; background-size: cover" data-height-lg="250" data-height-md="250" data-height-sm="200" data-height-xs="200" data-height-xxs="200">
 	<div class="container clearfix">
 		<div class="vertical-middle dark center">
-			<h1 class="white center"><?php echo $page_header; ?></h1>
+			<h1 class="white center">HEIT Symposia</h1>
 		</div>
 	</div>
 </section><!-- #page-title end -->
@@ -35,7 +34,7 @@ $heit_2016_text = get_field('heit_2016_text');				// Variable to store HEIT 2016
 	<div class="row clearfix common-height headerstick">
 
 		<!-- First column featuring background image -->
-		<div class="col-md-6 center col-padding filtered" style="background: url('<?php  bloginfo('template_url');  ?>/images/heit-2015-main-image-filter.jpg?>') center center no-repeat; background-size: cover" data-height-sm="250" data-height-xs="250" data-height-xxs="250">
+		<div class="col-md-6 center col-padding filtered" style="background: url(<?php echo $heit_2015_image; ?>) center center no-repeat; background-size: cover" data-height-sm="250" data-height-xs="250" data-height-xxs="250">
 		</div>
 
 		<!-- Second column featuring text -->
@@ -65,7 +64,7 @@ $heit_2016_text = get_field('heit_2016_text');				// Variable to store HEIT 2016
 		</div>
 
 		<!-- Second column featuring background image -->
-		<div class="col-md-6 center col-padding filtered" style="background: url('<?php  bloginfo('template_url');  ?>/images/uoit-night-filtered.jpg?>') center center no-repeat; background-size: cover" data-height-sm="250" data-height-xs="250" data-height-xxs="250">
+		<div class="col-md-6 center col-padding filtered" style="background: url(<?php echo $heit_2016_image; ?>) center center no-repeat; background-size: cover" data-height-sm="250" data-height-xs="250" data-height-xxs="250">
 		</div>
 	</div><!-- second past event section end -->
 
